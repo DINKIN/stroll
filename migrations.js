925164b7c00835b54486f5148980e50907d11a96
Fen Dweller <chemicalcrux@gmail.com>
2019-02-03 13:08:44 -0500

    Set up for v1.0 save version

--- migrations.js
+++ migrations.js
@@ -1,7 +1,9 @@
 // bumps save versions
 
 migrations = [
-
+  (save) => {
+    save.version = 1;
+  }
 ];
 
 function migrate(save, target=null) {
@@ -13,6 +15,12 @@ function migrate(save, target=null) {
 
   if (version == undefined) {
     alert("This save is from before versioning was added. It can't be automatically updated, and it might lose some settings. Double check that everything's there! Any subsequent saves will work correctly.");
+    save["version"] = migrations.length;
+    return false;
+  }
+
+  if (version == 0 ) {
+    alert("This save is from before v1.0. It can't be automatically migrated, so it may lose some settings. Double check the resulting character. Subsequent saves will function correctly.");
     save["version"] = migrations.length;
     return false;
   }
