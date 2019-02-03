82b670c74a220949202dd4469db35a8cf50221ec
Fen Dweller <chemicalcrux@gmail.com>
2019-02-03 13:09:05 -0500

    Bumped preset version number

--- presets.js
+++ presets.js
@@ -1,3 +1,3 @@
 let presets = [
-  {"version":0,"name":"Fen","brutality":"2","oralVore":true,"analVore":true,"analVoreToStomach":true,"hasTail":true,"tailType":"twisted","baseTailLength":2,"baseTailDiameter":0.4}
+  {"version":1,"name":"Fen","brutality":"2","oralVore":true,"analVore":true,"analVoreToStomach":true,"hasTail":true,"tailType":"twisted","baseTailLength":2,"baseTailDiameter":0.4}
 ]
